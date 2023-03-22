4bfe358e6a58297ac51438fe35d91f87d30f8391
svmitin <svmitin@outlook.com>
2023-03-22 18:55:04 +0000

    complete search on frontend

--- frontend/src/views/SearchResponseView.vue
+++ frontend/src/views/SearchResponseView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import { useFetch } from '@vueuse/core'
 import { RouterLink, RouterView } from 'vue-router'
 import Query from '../components/Query.vue'
 import Response from '../components/Response.vue'
@@ -11,22 +12,8 @@ const props = defineProps({
   }
 })
 const query = ref(props.search_text)
-const search_response = ref([
-  {
-    "url": "https://wooordhunt.ru/word/москва", 
-    "title": "Москва - перевод на английский, примеры, транскрипция", 
-    "description": "Перевод слова 'Москва' на английский - Moscow, Moskva", 
-    "indexed": "2023-03-14T19:47:29.419", 
-    "found_with": "links search"
-  }, 
-  {
-    "url": "https://wooordhunt.ru/word/\u043c\u043e\u0441\u043a\u0432\u0430", 
-    "title": "\u041c\u043e\u0441\u043a\u0432\u0430 - \u043f\u0435\u0440\u0435\u0432\u043e\u0434 \u043d\u0430 \u0430\u043d\u0433\u043b\u0438\u0439\u0441\u043a\u0438\u0439, \u043f\u0440\u0438\u043c\u0435\u0440\u044b, \u0442\u0440\u0430\u043d\u0441\u043a\u0440\u0438\u043f\u0446\u0438\u044f.", 
-    "description": "\u041f\u0435\u0440\u0435\u0432\u043e\u0434 \u0441\u043b\u043e\u0432\u0430 '\u041c\u043e\u0441\u043a\u0432\u0430' \u043d\u0430 \u0430\u043d\u0433\u043b\u0438\u0439\u0441\u043a\u0438\u0439 - Moscow, Moskva.", 
-    "indexed": "2023-03-14T19:47:29.419", 
-    "found_with": "full text search"
-  }
-])
+const { data } = useFetch('http://api.freedom:8000/search?query='+props.search_text).get().json()
+const search_response = data
 </script>
 
 <template>
@@ -38,7 +25,7 @@ const search_response = ref([
       <RouterLink to="/">back</RouterLink>
       
       <Response 
-        v-for="item in search_response"
+        v-for="item in search_response.pages"
         v-bind:url="item.url"
         v-bind:title="item.title"
         v-bind:description="item.description"
