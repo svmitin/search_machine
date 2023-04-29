9f7dfef974509311685167440c4d755e84fc4959
svmitin <svmitin@outlook.com>
2023-04-29 09:29:32 +0000

    update README.md & add site page

--- frontend/src/views/NewSiteView.vue
+++ frontend/src/views/NewSiteView.vue
@@ -1,29 +1,43 @@
 <script setup>
 import { ref } from 'vue'
+import { useFetch } from '@vueuse/core'
 import { RouterLink, RouterView } from 'vue-router'
 
 const site_url = ref('')
+const site_category = ref('')
+
+function register_site_and_get_integration_code(params) {
+  const { data } = useFetch('http://api.freedom:8000/add_site').post(
+    {
+      'site_url': site_url.value,
+      'site_category': site_category.value
+    }
+  ).json()
+  alert('Сайт добавлен на индексацию. Вот ваш код для интеграции на сайт. Его нужно поместить в блок HEAD каждой страницы сайта')
+}
+
 </script>
 
 <template>
   <div>
     <h3>Введите URL вашего сайта</h3>
-    <input v-model="site_url" @keyup.enter="$router.push({ name: 'search-response', params: {query: query} })">
-    <select>
-      <option>Игры</option>
-      <option>Реклама</option>
-      <option>Хобби</option>
-      <option>Музыка</option>
-      <option>Фидео и фильмы</option>
-      <option>18+</option>
-      <option>Автомобили</option>
-      <option>Блоги</option>
-      <option>Новости</option>
-      <option>Мода и красота</option>
-      <option>Здоровье</option>
-      <option>Общество</option>
+    <input v-model="site_url" @keyup.enter="register_site_and_get_integration_code()">
+    <select v-model="site_category">
+      <option value="games">Игры</option>
+      <option value="ad">Реклама</option>
+      <option value="hobby">Хобби</option>
+      <option value="music">Музыка</option>
+      <option value="movies">Фидео и фильмы</option>
+      <option value="18+">18+</option>
+      <option value="auto">Автомобили</option>
+      <option value="blogs">Блоги</option>
+      <option value="news">Новости</option>
+      <option value="beauty">Мода и красота</option>
+      <option value="health">Здоровье</option>
+      <option value="social">Общество</option>
+      <option value="shops">Магазины</option>
     </select>
-    <button @click="$router.push({ name: 'search-response', params: {query: query} })">Добавить</button>
+    <button @click="register_site_and_get_integration_code()">Добавить</button>
 
     <p>Ваш сайт будет добавлен в очередь на индексацию. На это может потребоваться некоторое время, но скорее 
     всего он будет проиндексирован за один час. Когда индексация будет завершена, вы сможете найти его в поисковой выдаче.</p>
